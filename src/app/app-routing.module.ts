c538f6c73ba2ad0d9d2943e4ae0d89a98ae42368
Pravallika <pravali9603@gmail.com>
2024-08-10 21:29:19 +0530

    10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShortsComponent } from './shorts/shorts.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { LibraryComponent } from './library/library.component';
+import { HistoryComponent } from './history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'shorts', component: ShortsComponent },
+  { path: 'subscriptions', component: SubscriptionsComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'history', component: HistoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { ShortsComponent } from './shorts/shorts.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { LibraryComponent } from './library/library.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    ShortsComponent,
+
+    LibraryComponent,
+    HistoryComponent,
+    SubscriptionsComponent
   ],
   imports: [
     BrowserModule,
